2e29ea15bdf7c14dcaf264d13f613fc8a7a26f18
DfrostG <paramate.2547@gmail.com>
2022-11-20 16:09:25 +0700

    Finalsomuch

--- main.py
+++ main.py
@@ -689,20 +689,20 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(65).render("Cosmic War", True, "#FFD700")
+        MENU_TEXT = get_font(65).render("Cosmic War", True, "#FFFFFF")
         MENU_RECT = MENU_TEXT.get_rect(center=(300, 100))
 
         PLAY_BUTTON = Button(image=pygame.image.load("itch/Play_Rect.png"), pos=(300, 250),
-                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color = (255,255,255))
+                             text_input="PLAY", font=get_font(75), base_color="#FFE484", hovering_color = (255,255,255))
         SCORE_BUTTON = Button(image=pygame.image.load("itch/Play_Rect.png"), pos=(300, 400),
-                              text_input="SCORE", font=get_font(75), base_color="#d7fcd4", hovering_color = (255,255,255))
+                              text_input="SCORE", font=get_font(75), base_color="#FFCC33", hovering_color = (255,255,255))
         QUIT_BUTTON = Button(image=pygame.image.load("itch/Play_Rect.png"), pos=(300, 550),
-                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color = (255,255,255))
+                             text_input="QUIT", font=get_font(75), base_color="#FC9601", hovering_color = (255,255,255))
 
         screen.blit(MENU_TEXT, MENU_RECT)
 
-        NAME_TEXT = get_font(25).render("Paramate Jiembanjong 65010594", True, "Orange")
-        NAME_RECT = NAME_TEXT.get_rect(center=(400, 670))
+        NAME_TEXT = get_font(25).render("Paramate Jiembanjong 65010594", True, "#D14009")
+        NAME_RECT = NAME_TEXT.get_rect(center=(300, 670))
 
         screen.blit(NAME_TEXT, NAME_RECT)
 
